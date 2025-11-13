7f390d655c48085a561a0b52a9da8421d738ee96
Allyzu <henriqueallyson18@gmail.com>
2025-11-12 23:09:56 -0300

    git commit -m "feat(auth): add password validation with bcrypt and update params type to Prisma.UsuarioCreateInput"

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,20 @@
+import {Prisma, Usuario } from '@prisma/client'
 import { UserService } from './../user/user.service';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private UserService: UserService) {}
 
-  
+    async signIn(params: Prisma.UsuarioCreateInput): Promise<any> {
+        const user = await this.UserService.user({ email: params.email });
+        if (!user) throw new NotFoundException('User not found');
+        
+        const passwordMatch = await bcrypt.compare(params.senha, user.senha);
+        if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+        
+        const { senha, ...result } = user;
+        return result;
+    }
 }
