bc3b0f22f84a35d2b45773bf9d7f4555aaf29c9d
Allyson Henrique <henriqueallyson18@gmail.com>
2025-11-16 00:30:25 -0300

    refactor(auth): improve signIn logic with clearer validation, bcrypt comparison and safe password removal; suppress unused vars warning

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,25 @@
-import {Prisma, Usuario } from '@prisma/client'
+import { Prisma } from '@prisma/client';
 import { UserService } from './../user/user.service';
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private UserService: UserService) {}
+  constructor(private UserService: UserService) {}
 
-    async signIn(params: Prisma.UsuarioCreateInput): Promise<any> {
-        const user = await this.UserService.user({ email: params.email });
-        if (!user) throw new NotFoundException('User not found');
-        
-        const passwordMatch = await bcrypt.compare(params.senha, user.senha);
-        if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
-        
-        const { senha, ...result } = user;
-        return result;
-    }
+  async signIn(params: Prisma.UsuarioCreateInput): Promise<any> {
+    const user = await this.UserService.user({ email: params.email });
+    if (!user) throw new NotFoundException('User not found');
+
+    const passwordMatch = await bcrypt.compare(params.senha, user.senha);
+    if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { senha, ...result } = user;
+    return result;
+  }
 }
